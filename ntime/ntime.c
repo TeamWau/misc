06ff728b1519264738ab1440ab6ba180a5d0516c
fauxm <fauxm@enfan.org>
2014-03-07 15:49:39 -0500

    Added basic argument support for ntime and added coloured output and an option to suppress it

--- ntime/ntime.c
+++ ntime/ntime.c
@@ -3,13 +3,16 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <stdint.h>
+#include <getopt.h>
 
-int64_t getTimeDiff(struct timespec *time_A, struct timespec *time_B){
-    return ((time_A->tv_sec * 1000000000) + time_A->tv_nsec) -
-           ((time_B->tv_sec * 1000000000) + time_B->tv_nsec);
+char colour;
+
+uint64_t getTimeDiff( struct timespec *time_A, struct timespec *time_B ) {
+    return ( ( time_A->tv_sec * 1000000000 ) + time_A->tv_nsec ) -
+           ( ( time_B->tv_sec * 1000000000 ) + time_B->tv_nsec );
 }
 
-int measuretime_coarse( char* program, char** program_args ) {
+int measureTime( char* program, char** program_args ) {
     struct timespec start, end;
     pid_t pID;
     int rs;
@@ -18,7 +21,7 @@ int measuretime_coarse( char* program, char** program_args ) {
         program_args[0] = " ";
     }
 
-    clock_gettime(CLOCK_MONOTONIC, &start);
+    clock_gettime( CLOCK_MONOTONIC, &start );
 
     pID = fork();
     if( pID == 0 ) {
@@ -30,23 +33,48 @@ int measuretime_coarse( char* program, char** program_args ) {
     else {
         waitpid( pID, &rs, 0 );
 
-        clock_gettime(CLOCK_MONOTONIC, &end);
+        clock_gettime( CLOCK_MONOTONIC, &end );
 
-        uint64_t tdiff = getTimeDiff(&end, &start);
+        uint64_t tdiff = getTimeDiff( &end, &start );
 
-        printf("\nApprox wall time: %llu ns\n", tdiff);
-        return 0;
+        if( colour == 'y' ) {
+            printf( "\n\033[31;1mntime approx. wall time result: \033[32m%llu\033[36mns\033[0m\n", tdiff );
+            return 0;
+        }
+        else if( colour == 'n' ) {
+            printf( "\nntime approx. wall time result: %lluns\n", tdiff );
+            return 0;
+        }
     }
 }
 
 int main( int argc, char **argv ) {
     if( argc == 1 ) {
-        printf( "%s - precise time program\nInvocation: %s <program> <args for program>\n", argv[0], argv[0] );
-    printf( "\nNOTICE: Times are _approximate_! As this is a very accurate timer, it measures the overhead time of its own execution, as well as any work done by the kernel.\nWhat this means is that the times are likely to vary heavily and should probably be averaged versus used as-is as a benchmark.\n" );
+        printf( "%s - precise time program\nInvocation: %s [-n] <program> <args for program>\n", argv[0], argv[0] );
+        printf( "Arguments for %s: \n'-n': disable coloured output.\n", argv[0] ); 
+        printf( "\nNOTICE: Times are \033[1mapproximate\033[0m! As this is a very accurate timer, it measures the overhead time of its own execution, as well as any work done by the kernel.\nWhat this means is that the times are likely to vary heavily and should probably be averaged versus used as-is as a benchmark.\n" );
     printf( "\nError: no program specified, terminating.\n" );
         return 1;
     }
+        int opt, flags;
+        opt = getopt( argc, argv, "n" );
+        switch( opt ){
+            case 'n':
+                flags = 1;
+                break;
+            default:
+                flags = 0;
+                break;
+            }
 
-        measuretime_coarse( argv[1], argv + 1 );
-        return 0;
+        if( flags == 0 ) {
+            colour = 'y';
+            measureTime( argv[1], argv + 1 );
+            return 0;
+        }
+        else if( flags == 1 ) {
+            colour = 'n';
+            measureTime( argv[2], argv + 2 );
+            return 0;
+        }
 }
