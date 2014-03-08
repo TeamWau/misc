96a82cd9e8195cbc4f982105ba03180072a19542
fauxm <fauxm@enfan.org>
2014-03-07 23:08:12 -0500

    added a professional code simulator

---
+++ problem.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* problem.c - professional code simulator */
+
+int problem() {
+    puts("Problem")
+
+
+
+
+    ;
+    return(1);
+} int main() {
+    while(problem() == 1 )
+    {
+        printf( "Problem\n");
+    }
+    return 2;}
+
