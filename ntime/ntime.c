ef42dff016f9bdba6e09dba7ff2f5ea05bce21be
Foamy <fauxm1135@gmail.com>
2014-04-01 18:05:50 -0400

    made a very small semantic change
    I really need to double check before I push next time

--- ntime/ntime.c
+++ ntime/ntime.c
@@ -4,7 +4,9 @@
  * TODO: (in order of importance)
  * 1. Have a clear separation of ntime's args and the given program's args ( currently, if you call a program with an arg, ntime will spit an error message that that arg isn't valid, and will run the program with that arg. This could be an issue if you pass the -v or -n arg to the program, as ntime will try to read those as its own args
  * 2. Comment code.
- * 3 Squash compiler warnings (about printing uint64_t's with the format string %llu)
+ * 3. Squash compiler warnings (about printing uint64_t's with the format string %llu)
+ * 
+ * Upon completion of TODO item 1, please change the version number to 1.0.0~ <3 Thanks --fauxm
  */
 #include <stdio.h>
 #include <time.h>
