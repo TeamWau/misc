f2818336b381b8af0adbb83bb5e473651fe6d037
fauxm <fauxm@teamwau.com>
2014-07-18 01:37:13 -0400

    Added StillBourne Shell

---
+++ sbsh/sbsh.c
@@ -0,0 +1,9 @@
+/*
+ * 05:31:24 < Suzuran> OH oh oh
+ * 05:31:28 < Suzuran> That's an even better idea
+ * 05:31:38 < Suzuran> Make a shell called the stillbourne shell, it exits -1 when called
+ */
+
+int main() { 
+    return -1;
+}
